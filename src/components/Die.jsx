75686225e53c5bb0cb98cbf11b8438a5264547a3
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-14 12:49:33 -0700

    add die faces using flex box

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -7,6 +7,75 @@ function Die(props) {
         backgroundColor: props.isHeld ? "#d3b08d" : "white"
     };
 
+    // Render the die face based on the value
+    var dieFace;
+    if (props.value === 1) {
+        dieFace = (
+            <div className="first-face">
+                <span className="pip"></span>
+            </div>
+        );
+    } else if (props.value === 2) {
+        dieFace = (
+            <div className="second-face">
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+        );
+    } else if (props.value === 3) {
+        dieFace = (
+            <div className="third-face">
+                <span className="pip"></span>
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+        );
+    } else if (props.value === 4) {
+        dieFace = (
+            <div className="fourth-face">
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+            </div>
+        );
+    } else if (props.value === 5) {
+        dieFace = (
+            <div className="fifth-face">
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+                <div className="column">
+                    <span className="pip"></span>
+                </div>
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+            </div>
+        );
+    } else {
+        dieFace = (
+            <div className="sixth-face">
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+                <div className="column">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>
+            </div>
+        );
+    }
+
     // Render the die component
     return (
         <div 
@@ -14,7 +83,7 @@ function Die(props) {
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            {dieFace}
         </div>
     );
 }
