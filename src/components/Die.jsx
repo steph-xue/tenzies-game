df75d6ecdf30fd8d45609dc51619548d84d83674
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-14 16:32:46 -0700

    update box shadow for held dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,11 +4,15 @@ function Die(props) {
     // Extract the value of the die from the props
     const dieValue = props.value;
 
-    // Change the background color of the die if it is held
+    // Change the background color and box shadow of the die if it is held
     const styles = {
         backgroundColor: props.isHeld ? "#d3b08d" : "white",
-        boxShadow: props.isHeld ? "inset 0 5px #d3b08d, inset 0 -5px #a88b6e, inset 5px 0 #c09f7 inset -5px 0 #c09f7f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7 inset -5px 0 #d7d7d7"
+        boxShadow: props.isHeld ? "inset 0 5px #d3b08d, inset 0 -5px #a88b6e, inset 5px 0 #c09f7f, inset -5px 0 #c09f7f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7"
     };
+    
+    /*
+    "inset 0 5px #d3b08d, inset 0 -5px #a88b6e, inset 5px 0 #c09f7, inset -5px 0 #c09f7f"
+    */
 
     // Generate pips (dots) for the die face based on the die's value
     const pips = (
