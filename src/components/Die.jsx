9e17d2aebb97c9e7079b49d3333637e29fd4618d
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-15 00:37:06 -0700

    add hover effect over dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,11 +4,11 @@ function Die(props) {
     // Extract the value of the die from the props
     const dieValue = props.value;
 
-    // Change the background color and box shadow of the die if it is held
-    const styles = {
-        backgroundColor: props.isHeld ? "#d3b08d" : "white",
-        boxShadow: props.isHeld ? "inset 0 5px #d3b08d, inset 0 -5px #a88b6e, inset 5px 0 #c09f7f, inset -5px 0 #c09f7f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7"
-    };
+    // Use state to manage styles
+    const [styles, setStyles] = React.useState({
+        backgroundColor: props.isHeld ? "#e5ccb0" : "white",
+        boxShadow: props.isHeld ? "inset 0 5px #e5ccb0, inset 0 -5px #a88b6e, inset 5px 0 #d5b28f, inset -5px 0 #d5b28f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7",
+    });
 
     // Generate pips (dots) for the die face based on the die's value
     const pips = (
@@ -17,12 +17,30 @@ function Die(props) {
             .map((pip, i) => <span key={i} className="pip" />)
         );
 
+    // Function to change the die's appearance when about to select (hover over) depending on whether it is held
+    function select() {
+        setStyles({
+            backgroundColor: props.isHeld ? "#e2c19b" : "rgb(235, 232, 232)",
+            boxShadow: props.isHeld ? "inset 0 5px #e5ccb0, inset 0 -5px #a88b6e, inset 5px 0 #d5b28f, inset -5px 0 #d5b28f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7",
+        });
+    }
+
+    // Function to change the die's appearance back to normal when not selecting (hover out) depending on whether it is held
+    function unselect() {
+        setStyles({
+            backgroundColor: props.isHeld ? "#e5ccb0" : "white",
+            boxShadow: props.isHeld ? "inset 0 5px #e5ccb0, inset 0 -5px #a88b6e, inset 5px 0 #d5b28f, inset -5px 0 #d5b28f" : "inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7",
+        });
+    }
+
     // Render the die component
     return (
         <div 
             className="die-face"
             style={styles}
             onClick={props.holdDice}
+            onMouseEnter={select}
+            onMouseLeave={unselect}
         >
             {pips}
         </div>
