3213ae1552ee0ee224022cd80ee7c64a69de67c1
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-15 02:13:56 -0700

    updated instructions for game

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,18 @@ function App() {
     // Create a state variable to store the number of rolls
     const [rolls, setRolls] = React.useState(0);
 
+    // Create a state variable to store the start time of the current game
+    const [startTime, setStartTime] = React.useState(null);
+
+    // Create a state variable to store the end time of the current game
+    const [endTime, setEndTime] = React.useState(null);
+
+    // Create a state variable to store the current time
+    const [time, setTime] = React.useState(null);
+
+    // Create a state variable to store the current score (time and number of rolls to finish game after achieving a tenzies win)
+    const [currentScore, setCurrentScore] = React.useState({});
+
     // Create a state variable to store the best score (fastest time and associated number of rolls)
     const [bestScore, setbestScore] = React.useState(() => localStorage.getItem("bestScore") || {});
     
@@ -28,15 +40,27 @@ function App() {
         const allSameValue = dice.every(die => die.value === firstValue);
         if (allHeld && allSameValue) {
             setTenzies(true);
+            setEndTime(new Date());
         }
     }, [dice])
 
+    // Check if the player has achieved a new best score (fastest time) to save to local storage
     React.useEffect(() => {
       if (tenzies) {
-        let currentScore = {
+        setCurrentScore({
           rolls: rolls,
-          time: new Date()
+          time: ((endTime.getTime() - startTime.getTime())/1000)
+        });
+      }
+
+      if (bestScore) {
+        if (currentScore.time < bestScore.time) {
+          setbestScore(currentScore);
+          localStorage.setItem("bestScore", currentScore);
         }
+      } else {
+        setbestScore(currentScore);
+        localStorage.setItem("bestScore", currentScore);
       }
       
     }, [tenzies])
@@ -78,18 +102,20 @@ function App() {
       setRolls(prevRolls => prevRolls + 1);
     }
 
-    // Function to start the game
+    // Function to start the game or start a new game
     function startGame() {
-      setGameStarted(true);
-    }
-
-    // Function to start a new game
-    function newGame() {
       setTenzies(false);
-      setGameStarted(false);
+      setGameStarted(true);
       setDice(allNewDice());
       setRolls(0);
+      setStartTime(new Date());
+
+      // Updates the current time every second
+      setInterval(() => {
+        setTime(new Date().getTime());
+      }, 1000);
     }
+
     
     // Map the dice objects to an array of dice components
     const diceElements = dice.map(die => (
@@ -106,27 +132,36 @@ function App() {
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. 
-            Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <div className="roll-number">
-              Number of rolls: {rolls}
-            </div>
-            
-            {tenzies && <h2 className="win">You won!</h2>}
-            {tenzies && <h2 className="roll-win">It took you {rolls} rolls!</h2>}
+            <p className="instructions-1">
+              The objective of the game is to roll the dice as fast as possible until all dice are the same.
+              The player who gets all dice on the same number and yells "Tenzi!" the fastest is the winner. 
+              You can play against others or try to beat your highest score!
+            </p>
+            <p className="instructions-2">
+              <p>» Click each die to freeze it at its current value between rolls.</p>
+              <p>» Click on the roll button to re-roll the unheld dice.</p>
+            </p>
 
             {
-              gameStarted && !tenzies &&
-                <button 
-                    className="tenzies-button" 
-                    onClick={rollDice}
-                >
-                    Roll
-                </button>
+              gameStarted &&
+              <div>
+                {bestScore !== null && <h2 className="best-score">Best Score: {bestScore.time} seconds with {bestScore.rolls} rolls</h2>}
+                <div className="dice-container">
+                  {diceElements}
+                </div>
+                <div className="roll-number">
+                  Number of rolls: {rolls}
+                </div>
+                <div className="current-time">
+                  Current time: {time/1000} seconds
+                </div>
+              </div>   
             }
+
+            {tenzies && <h2 className="win">You won!</h2>}
+            {tenzies && <h2 className="roll-win">It took you {currentScore.time} seconds!</h2>}
+            {tenzies && <h2 className="roll-win">It took you {rolls} rolls!</h2>}
+
             {
               !gameStarted && !tenzies &&
                 <button 
@@ -137,10 +172,19 @@ function App() {
                 </button>
             }
             {
+              gameStarted && !tenzies &&
+                <button 
+                    className="tenzies-button" 
+                    onClick={rollDice}
+                >
+                    Roll
+                </button>
+            }
+            {
               gameStarted && tenzies &&
                 <button 
                     className="tenzies-button" 
-                    onClick={newGame}
+                    onClick={startGame}
                 >
                     New Game
                 </button>
